0dc7676745f400951c380399a973789586e362db
Izyl Canonicato <ifcanonicato@gmail.com>
2020-04-13 21:04:31 -0700

    Compiled ExecUnit, uncompiled ShiftUnit

--- pt1/ExU/SourceCode/ExecUnit.vhd
+++ pt1/ExU/SourceCode/ExecUnit.vhd
@@ -5,18 +5,57 @@ use ieee.numeric_std.all;
 Entity ExecUnit is
 	Generic ( N : natural := 64 );
 	Port ( A, B : in std_logic_vector( N-1 downto 0 );
-	-- Control signals
-		NotA : in std_logic := '0';
-		FuncClass : in std_logic_vector( 1 downto 0 );
-		LogicFN : in std_logic_vector( 1 downto 0 );
-		ShiftFN : in std_logic_vector( 1 downto 0 );
-		AddnSub, ExtWord : in std_logic := '0';
-	-- Output
-		Y : out std_logic_vector( N-1 downto 0 );
-	-- Status signals
-		Zero, AltB, AltBu : out std_logic );
+	NotA : in std_logic := '0';
+	FuncClass : in std_logic_vector( 1 downto 0 );
+	LogicFN : in std_logic_vector( 1 downto 0 );
+	ShiftFN : in std_logic_vector( 1 downto 0 );
+	AddnSub, ExtWord : in std_logic := '0';
+	Y : out std_logic_vector( N-1 downto 0 );
+	Zero, AltB, AltBu : out std_logic
+);
 End Entity ExecUnit;
 
+
 architecture rtl of ExecUnit is
-begin
+	signal y_arith, y_logic, y_shift, sum, AltB_sig, AltBu_sig: std_logic_vector(N-1 downto 0);
+	-- ArithUnit 
+	signal FuncClassSel : std_logic_vector(1 downto 0); 
+	signal AltB_flag, AltBu_flag: std_logic; 
+
+begin	
+ -- instantiate ArithUnit entity
+	-- omits some output signals 
+	arithUnit: entity work.ArithUnit port map (A => A, B => B, Y => y_arith, NotA => NotA, AddnSub => AddnSub, ExtWord => ExtWord, Zero => Zero, AltB => AltB_flag, AltBu => AltB_flag);
+
+	AltB <= AltB_flag;
+	AltBu <= AltBu_flag; 
+	process(AltB_flag, AltBu_flag)
+	begin
+		if(AltB_flag = '1') then
+			AltB_sig <= (others => '0'); 
+			AltB_sig(0) <= AltB_flag; 
+			--AltB_sig <= resize(AltB_sig, N);  
+		end if; 
+		if(AltBu_flag = '1') then
+			AltBu_sig <= (others => '0'); 
+			AltBu_sig(0) <= AltBu_flag; 
+			--AltBu_sig <= AltBu_flag & "0"; 
+		end if; 
+	end process; 
+-- instantiate shiftUnit entity 	
+	-- C is the output Y from ArithUnit 
+
+	shiftUnit: entity work.ShiftUnit port map (A => A, B => B, C => y_arith, Y => y_shift, ShiftFN => ShiftFN, ExtWord => ExtWord); 
+	
+-- instantiate logicUnit entity 
+	
+	logicUnit: entity work.LogicUnit port map (A => A, B => B, Y => y_logic, LogicFN => LogicFN); 
+	
+	with FuncClassSel select 
+		Y <= AltBu_sig when "00", 
+			AltB_sig when "01", 
+			y_shift when "10", 
+			y_logic when others; 
+
+	
 end architecture rtl;
--- pt1/ExU/SourceCode/ShiftUnit.vhd
+++ pt1/ExU/SourceCode/ShiftUnit.vhd
@@ -5,13 +5,74 @@ use ieee.numeric_std.all;
 Entity ShiftUnit is
 	Generic ( N : natural := 64 );
 	Port ( A, B, C : in std_logic_vector( N-1 downto 0 );
-	-- Output
-		Y : out std_logic_vector( N-1 downto 0 );
-	-- Control signals
-		ShiftFN : in std_logic_vector( 1 downto 0 );
-		ExtWord : in std_logic );
+	Y : out std_logic_vector( N-1 downto 0 );
+	ShiftFN : in std_logic_vector( 1 downto 0 );
+	ExtWord : in std_logic );	
 End Entity ShiftUnit;
 
 architecture rtl of ShiftUnit is
-begin
-end architecture rtl;
+	signal y_a, y_b, y_c, sll_out, srl_out, sra_out : std_logic_vector(N-1 downto 0);
+	signal swappedA, swappedimmd1, swappedimmd3 : std_logic_vector(N-1 downto 0); 
+	signal ShiftCount : std_logic_vector(5 downto 0); 
+	signal immd1, immd2, immd3, immd4 : std_logic_vector(N-1 downto 0); 
+	signal inputASel : std_logic;
+begin	
+	
+	-- Input select bits 
+	inputASel <= ShiftFN(1) AND ExtWord;
+	
+	-- Input B extract 
+	ShiftCount <= B(5 downto 0); 
+	
+	-- Swap Word
+	swappedA(N-1 downto (N/2)) <= A((N/2)-1 downto 0); 
+	swappedA((N/2)-1 downto 0) <= "0"; 
+	
+	-- Input into 64-bit barrel shifter
+	with inputASel select 
+	y_a <= A when '0', 
+			swappedA when others; 
+			
+	-- instantiate Shifter entities 
+	sll_op: entity work.SLL64 port map (X => y_a, Y => sll_out, ShiftCount => ShiftCount); 
+	
+	srl_op: entity work.SLL64 port map (X => y_a, Y => sll_out, ShiftCount => ShiftCount); 
+	
+	sra_op: entity work.SLL64 port map (X => y_a, Y => sll_out, ShiftCount => ShiftCount); 
+	
+	-- Lower bits
+	process(sll_out)
+	begin
+		with ShiftFN(0) select 
+			immd1 <= C when '0',  
+				sll_out when others; 
+		
+		swappedimmd1(N-1 downto (N/2)) <= immd1((N/2)-1 downto 0); 
+		swappedimmd1((N/2)-1 downto 0) <= '0'; 
+		
+		with ExtWord select 
+			immd2 <= immd1 when '0', 
+				swappedimmd1 when others; 
+		
+	end process; 
+		
+	-- Upper bits
+	process(srl_out, sra_out)
+	begin
+		with ShiftFN(0) select 
+				immd3 <= srl_out when '0',  
+					sra_out when others; 
+		
+		swappedimmd3(N-1 downto (N/2)) <= immd3((N/2)-1 downto 0); 
+		swappedimmd3((N/2)-1 downto 0) <= '0'; 
+		
+		with ExtWord select 
+			immd4 <= immd1 when '0', 
+				swappedimmd3 when others; 
+	end process; 
+	
+	with ShiftFN(1) select
+		Y <= immd2 when '0', 
+				immd4 when others; 
+		
+end architecture rtl; 
