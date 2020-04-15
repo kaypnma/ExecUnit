504daa52290f78ed341f759d672cc36efc68ab65
Izyl Canonicato <ifcanonicato@gmail.com>
2020-04-15 16:01:31 -0700

    update Shift Unit design

--- pt1/ExU/SourceCode/ShiftUnit.vhd
+++ pt1/ExU/SourceCode/ShiftUnit.vhd
@@ -12,9 +12,11 @@ End Entity ShiftUnit;
 
 architecture rtl of ShiftUnit is
 	signal y_a, y_b, y_c, sll_out, srl_out, sra_out : std_logic_vector(N-1 downto 0);
-	signal swappedA, swappedimmd1, swappedimmd3 : std_logic_vector(N-1 downto 0); 
+	signal swappedA, swappedinter1, swappedinter3 : std_logic_vector(N-1 downto 0); 
 	signal ShiftCount : std_logic_vector(5 downto 0); 
-	signal immd1, immd2, immd3, immd4 : std_logic_vector(N-1 downto 0); 
+
+--changed to inter
+	signal inter1, inter2, inter3, inter4 : std_logic_vector(N-1 downto 0); 
 	signal inputASel : std_logic;
 begin	
 	
@@ -26,11 +28,11 @@ begin
 	
 	-- Swap Word
 	swappedA(N-1 downto (N/2)) <= A((N/2)-1 downto 0); 
-	swappedA((N/2)-1 downto 0) <= "0"; 
+	swappedA((N/2)-1 downto 0) <= (others => '0'); 
 	
 	-- Input into 64-bit barrel shifter
 	with inputASel select 
-	y_a <= A when '0', 
+		y_a <= A when '0', 
 			swappedA when others; 
 			
 	-- instantiate Shifter entities 
@@ -43,36 +45,44 @@ begin
 	-- Lower bits
 	process(sll_out)
 	begin
-		with ShiftFN(0) select 
-			immd1 <= C when '0',  
-				sll_out when others; 
+		if(ShiftFN(0) = '0') then
+			inter1 <= C;
+		else 
+			inter1 <= sll_out; 
+		end if; 
 		
-		swappedimmd1(N-1 downto (N/2)) <= immd1((N/2)-1 downto 0); 
-		swappedimmd1((N/2)-1 downto 0) <= '0'; 
-		
-		with ExtWord select 
-			immd2 <= immd1 when '0', 
-				swappedimmd1 when others; 
+		swappedinter1(N-1 downto (N/2)) <= inter1((N/2)-1 downto 0); 
+		swappedinter1((N/2)-1 downto 0) <= (others => '0'); 
+
+		if(ExtWord = '0') then
+			inter2 <= inter1;
+		else 
+			inter2 <= swappedinter1; 
+		end if; 
 		
 	end process; 
 		
 	-- Upper bits
 	process(srl_out, sra_out)
 	begin
-		with ShiftFN(0) select 
-				immd3 <= srl_out when '0',  
-					sra_out when others; 
+		if(ShiftFN(0) = '0') then
+			inter3 <= srl_out;
+		else 
+			inter3 <= sra_out; 
+		end if; 
 		
-		swappedimmd3(N-1 downto (N/2)) <= immd3((N/2)-1 downto 0); 
-		swappedimmd3((N/2)-1 downto 0) <= '0'; 
-		
-		with ExtWord select 
-			immd4 <= immd1 when '0', 
-				swappedimmd3 when others; 
+		swappedinter3(N-1 downto (N/2)) <= inter3((N/2)-1 downto 0); 
+		swappedinter3((N/2)-1 downto 0) <= (others => '0'); 
+
+		if(ExtWord = '0') then
+			inter4 <= inter3;
+		else 
+			inter4 <= swappedinter3; 
+		end if; 
 	end process; 
 	
 	with ShiftFN(1) select
-		Y <= immd2 when '0', 
-				immd4 when others; 
+		Y <= inter2 when '0', 
+				inter4 when others; 
 		
 end architecture rtl; 
